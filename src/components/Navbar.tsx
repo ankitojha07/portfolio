874204479f01928be6642a8808a0602a961b37ee
ankitojha07 <ankitojha1409@gmail.com>
2024-11-21 11:39:10 +0530

    navbar feature added and tag button color update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,41 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <div className="flex flex-col gap-6">
       <ul className="nav flex flex-row gap-8 justify-end items-center font-bold text-xl">
-        <li>About</li>
-        <li>Resume</li>
-        <li>Projects</li>
+        <li>
+          <Link href="/">About</Link>
+          <div
+            className={`${
+              pathname === "/" ? "h-1 bg-teal-600 rounded-full" : " hidden"
+            } `}
+          ></div>
+        </li>
+        <li>
+          <Link href="/resume">Resume</Link>
+          <div
+            className={`${
+              pathname === "/resume"
+                ? "h-1 bg-teal-600 rounded-full"
+                : " hidden"
+            } `}
+          ></div>
+        </li>
+        <li>
+          <Link href="/projects">Projects</Link>
+          <div
+            className={`${
+              pathname === "/projects"
+                ? "h-1 bg-teal-600 rounded-full"
+                : " hidden"
+            } `}
+          ></div>
+        </li>
       </ul>
     </div>
   );
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,6 +1,6 @@
 export default function Tags({ title }: { title: string }) {
   return (
-    <p className="bg-zinc-900 py-2 text-white-800 rounded-full border-gray-600 border px-4 text-sm w-full">
+    <p className="bg-zinc-900 py-2 text-white-800 rounded-full border px-4 text-md w-full border-teal-800">
       {title}
     </p>
   );
