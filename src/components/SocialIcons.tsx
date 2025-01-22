40d393df50d16b55a34f741f0f95e62c1f2e28ef
ANkit Ojha <ankitojha1409@gmail.com>
2025-01-23 00:56:27 +0530

    social profiles updated

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,13 +5,17 @@ import {
   IoLogoYoutube,
   IoLogoInstagram,
   IoLogoWhatsapp,
+  IoLogoTwitter,
 } from "react-icons/io";
 
 const SocialIcons = () => {
   return (
     <div className="flex justify-center items-center gap-5 text-2xl text-teal-600">
-      <a href="https://www.youtube.com/@Codewith_Coffee" target="_blank">
+      <a href="https://www.youtube.com/@guylovescoding" target="_blank">
         <IoLogoYoutube />
+      </a>
+      <a href="https://www.x.com/guylovescoding" target="_blank">
+        <IoLogoTwitter />
       </a>
       <a href="https://github.com/ankitojha07" target="_blank">
         <IoLogoGithub />
