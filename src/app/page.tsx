5822763bebdd59d3a6529e240ad6564ab4fd2fa2
ANkit Ojha <ankitojha1409@gmail.com>
2025-01-23 00:52:46 +0530

    Skills section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,17 +34,20 @@ const Home = () => {
   return (
     <div className="flex flex-col justify-start gap-8">
       <p className="text-justify tracking-wide text-md font-sans">
-        Innovative and results-driven Full Stack Developer with expertise in
-        MERN stack development, building scalable and user-friendly web
-        applications. Proficient in designing and implementing robust solutions
-        across frontend and backend, ensuring seamless user experiences and
-        operational efficiency. Hands-On experience in developing secure
-        authentication systems, dynamic APIs, and responsive UI components.
-        Adept at integrating modern technologies to solve real-world problems,
-        streamline workflows, and enhance application performance. Proven
-        ability to translate complex business requirements into scalable,
-        maintainable, and impactful digital products, driving success in diverse
-        projects.
+        Forward-thinking Frontend Web Developer proficient in{" "}
+        <span className="text-teal-600 font-bold italic">
+          {" "}
+          React.js, Next.js, Tailwind CSS, and TypeScript{" "}
+        </span>
+        , with a strong focus on crafting dynamic, user-centric interfaces and
+        optimizing web performance. Experienced in implementing responsive
+        designs, integrating{" "}
+        <span className="text-teal-600 font-bold italic">RESTful APIs </span>,
+        and leveraging modern tools like Webpack and Vite to build scalable and
+        maintainable solutions. Skilled in transforming Figma designs into
+        seamless, interactive web applications tailored to user needs. Excels in
+        collaborating with cross-functional teams to deliver robust and
+        efficient digital experiences
       </p>
       <p className="font-bold text-start text-xl">What I am doing?</p>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -59,11 +62,9 @@ const Home = () => {
 
       <p className="font-bold text-start text-xl">More info?</p>
       <div className="text-zinc-400 flex flex-col gap-2">
-        <p className="text-start ">Abhi ke liye bs itna hi.... 🫡</p>
-        <p className="text-start">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
-          quisquam!
-        </p>
+        <p className="text-start ">that`s it 🫡</p>
+        <p className="text-start">Feel free to reach out for anything : )</p>
+        {/*
         <p className="text-start">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia,
           quisquam!
@@ -72,14 +73,11 @@ const Home = () => {
           Kuch bhi add kr rha hu just to fill the gap....
         </p>
         <p className="text-start">
-          Upar jo profile summary hai wo bhi copy pase kiya hu 😂
-        </p>
-        <p className="text-start">
           Ek baat aor: Koi job referral hoga to please btana jaroor : )
         </p>
         <p className="text-start">
           Don`t forget to check the project page... wo jaroori hai
-        </p>
+        </p> */}
       </div>
     </div>
   );
