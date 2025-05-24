ab2bb47e1f3fbcc5a0c3f0e3a7351e33322f34eb
ankitojha07 <ankitojha1409@gmail.com>
2025-05-24 17:47:24 +0530

    updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="flex flex-col gap-6">
       <ul className="nav flex flex-row gap-4 md:gap-8 justify-center md:justify-end items-center font-bold md:text-xl">
         <li>
-          <Link href="/">About</Link>
+          <Link href="/">Home</Link>
           <div
             className={`${
               pathname === "/" ? "h-1 bg-teal-600 rounded-full" : " hidden"
@@ -17,7 +17,7 @@ const Navbar = () => {
           ></div>
         </li>
         <li>
-          <Link href="/resume">Resume</Link>
+          <Link href="/resume">About Me</Link>
           <div
             className={`${
               pathname === "/resume"
